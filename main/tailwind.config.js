6887325858ba6141955c87be61c645b144507ad6
Feiron <53085628+fr1g@users.noreply.github.com>
2023-08-01 21:21:05 +0800

    awa

---
+++ main/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,css,js}",
+    "./order/*/*.{html,css,js}"
+  ],
+  theme: {
+    extend: {
+        
+    },
+  },
+  plugins: [],
+}
